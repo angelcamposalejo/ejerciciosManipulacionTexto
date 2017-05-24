f124c2bf09e670f9104c37935e5c9d22a16717ce
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-24 17:49:23 -0500

    ejercicio de cadenas utilizando wchar_t

--- ManipulacionTexto/ManipulacionTexto.cpp
+++ ManipulacionTexto/ManipulacionTexto.cpp
@@ -8,6 +8,8 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR cmdLine, int cmdSho
 
 void ManipulacionTexto::Window_Open(Win::Event& e)
 {
+	this->radioNumero.Checked = true;
+	this->radioLetra.Checked = false;
 
 }
 
@@ -15,16 +17,56 @@ void ManipulacionTexto::Window_Open(Win::Event& e)
 
 void ManipulacionTexto::btCalcular_Click(Win::Event& e)
 {
-	wstring entrada = tbxEntrada.Text;
-	const int len = entrada.length();
-	int contador = 0;
-	for (int i = 0; i < len; i++)
+	if (radioNumero.Checked == true)
 	{
-		if (entrada[i] == 'a')
+		tbxSalida.Text = GetDayByNumb(tbxEntrada.IntValue);
+	}
+	else
+	{
+		wstring entrada = tbxEntrada.Text;
+		if (entrada.length() == 1)
 		{
-			contador++;
+			tbxSalida.Text = GetDayByChar(entrada[0]);
 		}
 	}
-	tbxString.IntValue = contador;
 }
-
+wchar_t* ManipulacionTexto::GetDayByChar(wchar_t entrada)
+{
+	switch (entrada)
+	{
+	case 'S':
+		return L"Sunday";
+	case 'M':
+		return L"Monday";
+	case 'T':
+		return L"Tuesday";
+	case 'W':
+		return L"Wednesday";
+	case 'H':
+		return L"Thursday";
+	case 'F':
+		return L"Friday";
+	case 'A':
+		return L"Saturday";
+	}
+}
+wchar_t* ManipulacionTexto::GetDayByNumb(int entrada)
+{
+	switch (entrada)
+	{
+	case 0:
+		return L"Sunday";
+	case 1:
+		return L"Monday";
+	case 2:
+		return L"Tuesday";
+	case 3:
+		return L"Wednesday";
+	case 4:
+		return L"Thursday";
+	case 5:
+		return L"Friday";
+	case 6:
+		return L"Saturday";
+	}
+}
--- ManipulacionTexto/ManipulacionTexto.h
+++ ManipulacionTexto/ManipulacionTexto.h
@@ -9,37 +9,47 @@ public:
 	~ManipulacionTexto()
 	{
 	}
-
+	wchar_t* GetDayByChar(wchar_t entrada);
+	wchar_t* GetDayByNumb(int entrada);
 protected:
 	//______ Wintempla GUI manager section begin: DO NOT EDIT AFTER THIS LINE
 	Win::Label lb1;
 	Win::Textbox tbxEntrada;
 	Win::Button btCalcular;
 	Win::Label lb2;
-	Win::Textbox tbxString;
+	Win::Textbox tbxSalida;
+	Win::Button gbox1;
+	Win::Button radioNumero;
+	Win::Button radioLetra;
 protected:
 	Win::Gdi::Font fontArial014A;
 	void GetDialogTemplate(DLGTEMPLATE& dlgTemplate)
 	{
-		dlgTemplate.cx = Sys::Convert::PixelToDlgUnitX(279);
-		dlgTemplate.cy = Sys::Convert::PixelToDlgUnitY(82);
+		dlgTemplate.cx = Sys::Convert::PixelToDlgUnitX(369);
+		dlgTemplate.cy = Sys::Convert::PixelToDlgUnitY(114);
 		dlgTemplate.style = WS_CAPTION | WS_POPUP | WS_SYSMENU | WS_VISIBLE | DS_CENTER | DS_MODALFRAME;
 	}
 	//_________________________________________________
 	void InitializeGui()
 	{
 		this->Text = L"ManipulacionTexto";
-		lb1.Create(NULL, L"Sentencia", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 6, 4, 68, 25, hWnd, 1000);
-		tbxEntrada.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 74, 4, 198, 25, hWnd, 1001);
-		btCalcular.Create(NULL, L"Calcular", WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_PUSHBUTTON | BS_CENTER | BS_VCENTER, 157, 47, 77, 28, hWnd, 1002);
-		lb2.Create(NULL, L"Count", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 7, 48, 48, 25, hWnd, 1003);
-		tbxString.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 55, 49, 89, 25, hWnd, 1004);
+		lb1.Create(NULL, L"Entrada", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 145, 4, 68, 25, hWnd, 1000);
+		tbxEntrada.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 213, 4, 52, 25, hWnd, 1001);
+		btCalcular.Create(NULL, L"Calcular", WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_PUSHBUTTON | BS_CENTER | BS_VCENTER, 285, 4, 77, 28, hWnd, 1002);
+		lb2.Create(NULL, L"Salida", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 146, 48, 48, 25, hWnd, 1003);
+		tbxSalida.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 194, 49, 89, 25, hWnd, 1004);
+		gbox1.Create(WS_EX_TRANSPARENT, L"Entrada", WS_CHILD | WS_VISIBLE | BS_GROUPBOX, 8, 9, 133, 98, hWnd, 1005);
+		radioNumero.Create(NULL, L"Numero", WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_AUTORADIOBUTTON | BS_LEFT | BS_VCENTER, 21, 30, 111, 26, hWnd, 1006);
+		radioLetra.Create(NULL, L"Letra", WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_AUTORADIOBUTTON | BS_LEFT | BS_VCENTER, 20, 55, 112, 26, hWnd, 1007);
 		fontArial014A.Create(L"Arial", 14, false, false, false, false);
 		lb1.Font = fontArial014A;
 		tbxEntrada.Font = fontArial014A;
 		btCalcular.Font = fontArial014A;
 		lb2.Font = fontArial014A;
-		tbxString.Font = fontArial014A;
+		tbxSalida.Font = fontArial014A;
+		gbox1.Font = fontArial014A;
+		radioNumero.Font = fontArial014A;
+		radioLetra.Font = fontArial014A;
 	}
 	//_________________________________________________
 	void btCalcular_Click(Win::Event& e);
